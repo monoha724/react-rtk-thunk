e4b77ba8d991b991440600a208be7dc04966f798
monoha724 <bunnyalex724@gmail.com>
2023-03-16 22:32:38 +0900

    회원가입, 로그인 구현 시작

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/detail/:id" element={<Detail />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Cards from '../components/Cards';
 import Uploader from '../components/Uploader';
 import '../style.css';
 
 function Home() {
+  const navi = useNavigate();
   const [modal, setModal] = useState(false);
 
   return (
     <div className='home-container'>
       <div className='headBanner'>
         <h1 className='headerTitle'>MONOHA GALLERY</h1>
+        <button onClick={()=>{navi("/login")}}>로그인</button>
+        <button onClick={()=>{navi("/register")}}>회원가입</button>
         {modal ? 
           <button onClick={()=>{setModal(!modal)}}>닫기</button> : 
           <button onClick={()=>{setModal(!modal)}}>글쓰기</button>}
